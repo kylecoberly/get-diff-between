8131ffccdd5687f54093473be2b027ddf7785680
Kyle Coberly <kyle.coberly@gmail.com>
2018-06-17 15:53:00 -0600

    Refactor removal

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 module.exports = function getDiffBetween(oldArray, newArray){
     return oldArray.reduce((diffArray, oldElement) => {
         const matchedElement = newArray.find(newElement => oldElement.id == newElement.id);
-        if (!matchedElement){
-            diffArray.push({
-                type: "REMOVE",
-                id: oldElement.id
-            });
-        }
+        if (!matchedElement) addRemoval(diffArray, oldElement.id);
+
         return diffArray;
     },[]);
+}
+
+function addRemoval(array, id){
+    array.push({
+        type: "REMOVE",
+        id: id
+    });
+    return array;
 }
